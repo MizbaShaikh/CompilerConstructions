e5cdfb861a138e34ce1614aece71b4bd308de132
Mizba Shaikh <16co32@aitdgoa.edu.in>
2019-07-15 15:54:19 +0530

    first c program

---
+++ expt1/expt1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+int main()
+{
+	
+	char array[20];
+	int i,n;
+	
+	printf("Enter the size of the string\n");
+	scanf("%d",&n);
+	
+	
+	printf("Enter the characters\n");
+	
+	for(i=1;i<=n;i++)
+	{	
+	scanf("%c",&array[i]);
+	}
+
+for(i=1;i<=n;i++)
+{
+	
+	printf("%c",array[i]);
+}
+return 0;
+}
